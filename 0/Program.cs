e20b252abd0dc22fcc83155bb0bc240b654d07fc
Виктор <viktor.avgn@yandex.ru>
2023-02-25 21:29:48 +0700

    задача 0, поправил

--- 0/Program.cs
+++ 0/Program.cs
@@ -2,11 +2,11 @@
 Console.Write("Введите число: ");
 int number=int.Parse(Console.ReadLine());
 //Решение математическим путем
-double sqr=3.3*3.3;
+double sqr= number * number;
 Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
 //Решение с использованием библиотеки
 int sqr1=Convert.ToInt32(Math.Pow(number,2));
 Console.WriteLine($"Квадрат числа {number} равен = {sqr1}");
-double number2 = 3.3;
+double number2 = number;
 double sqr2=(Math.Pow(number2, 2));
 Console.WriteLine($"Квадрат числа {number2} равен = {sqr2}");
