5656eec39bd08f8ccd9b4cbff6b734f597b22058
Виктор <viktor.avgn@yandex.ru>
2023-03-10 13:44:44 +0700

    Z18

---
+++ 0/18/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+
+switch (quarter)
+{
+    case 1:{
+        Console.WriteLine("x>0 y>0");
+        break;
+        }
+    case 2:{
+        Console.WriteLine("x<0 y>0");
+        break;
+        }
+    case 3:{
+        Console.WriteLine("x<0 y<0");
+        break;
+        }
+    case 4:{
+        Console.WriteLine("x>0 y<0");
+        break;
+        }
+    default:{
+        Console.WriteLine("Введена неправильная четверть");
+        break;
+        }
+}
+Console.WriteLine("Введена неправильная четверть");
+// if (quarter == 1)
+//     Console.WriteLine("");
+// else if (quarter == 2)
+//     Console.WriteLine("");
+// else if (quarter == 3)
+//     Console.WriteLine("");
+// else if (quarter == 4)
+//     Console.WriteLine("");
+// else
+//     Console.WriteLine("");
