cf2101ced6921d8f9c8f7effc0e892e6dba2f17a
Виктор <viktor.avgn@yandex.ru>
2023-03-16 10:29:54 +0700

    z26

---
+++ 0/26/Program.cs
@@ -0,0 +1,27 @@
+﻿// 26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int s = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр = {CountNumbers(s)}");
+
+
+int CountNumbers(int n) //123
+{
+    int count = 0; // count 0
+
+    if (n < 0)
+        n *= (-1); // n = n * (-1) 
+    while (n > 0) // 0 > 0
+    {
+        n /= 10; // n = n / 10    n = 0
+        count++; // count 3
+    }
+    //double a = Math.Pow(123, 2);
+    return count;
+}
