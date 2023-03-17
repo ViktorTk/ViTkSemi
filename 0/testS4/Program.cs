71d059abe5f7574a4480dc844d7ab85dae507988
Виктор <viktor.avgn@yandex.ru>
2023-03-17 10:55:15 +0700

    testS4

---
+++ 0/testS4/Program.cs
@@ -0,0 +1,26 @@
+﻿//Является ли число палиндромом
+
+Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txt = number.Replace(" ", "");  /// для проверки текста на палиндром
+
+bool Pal()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+if (Pal())
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
