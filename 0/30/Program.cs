4c9aaab6b7b016e7cb7056f55f1a931c88f18cfc
Виктор <viktor.avgn@yandex.ru>
2023-03-17 09:22:34 +0700

    Z30

---
+++ 0/30/Program.cs
@@ -0,0 +1,28 @@
+﻿//30 Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+
+//[1,0,1,1,0,1,0,0]
+// a = new Random().Next()
+// Random r = new Random();
+// int a = new Random().Next(2, 10)
+// int a = r.Next(2, 10);
+// int a = r.Next(2);
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = 8;
+int[] array = GetBinArray(num);
+Console.Write($"[{String.Join(", ", array)}]");
+Console.Write($"[");
+for (int i = 0; i < num - 1; i++){
+    Console.Write($"{array[i]}, ");
+}Console.WriteLine($"{array[num - 1]}]");
+
+
+int[] GetBinArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++){
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
